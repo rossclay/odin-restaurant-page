a038ea58a711950e8b57e68464ac462dbe3f26cf
Ross Clay <clay.rossclay@gmail.com>
2024-06-10 20:02:04 -0400

    initialize about and menu modules

---
+++ src/about.js
@@ -0,0 +1,10 @@
+function createAboutPage() {
+    content = document.getElementById('content')
+    const contentHeader = document.querySelector('.content-header')
+    contentHeader.innerHTML = 'About'
+
+    let aboutCard = document.createElement('div')
+
+}
+
+export { createAboutPage }
---
+++ src/menu.js
@@ -0,0 +1,10 @@
+function createMenuPage() {
+    content = document.getElementById('content')
+    const contentHeader = document.querySelector('.content-header')
+    contentHeader.innerHTML = 'Menu'
+
+    let menuCard = document.createElement('div')
+
+}
+
+export { createMenuPage }
