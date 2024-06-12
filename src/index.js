7eb8dbff82b8668fb997330ded482a1f82c05bdb
Ross Clay <clay.rossclay@gmail.com>
2024-06-12 12:38:02 -0400

    adjusts event listeners

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 console.log(`i'm doing something!`)
 console.log(`and again!`)
+
+const navBtns = document.querySelectorAll('.nav-btn')
+
+let currentPage
+
+function handleClick(e) {
+    const targetPage = e.target
+
+    updatePage(targetPage)
+}
+
+
+function updatePage(pageOption) {
+
+}
+
+navBtns.forEach((navBtn) => navBtn.addEventListener('click', handleClick))
