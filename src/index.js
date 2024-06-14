d553b472bdf9dad9275f02e925758c0870de6956
Ross Clay <clay.rossclay@gmail.com>
2024-06-14 13:21:47 -0400

    updates module imports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import './style.css';
+import { createMenuDOM } from './menu';
+import { createHomeDOM } from './create';
+import { createContactDOM } from './contact';
+
+// createHomeDOM()
+
+// createContactDOM()
+
+createMenuDOM()
+
 console.log(`i'm doing something!`)
 console.log(`and again!`)
 
@@ -5,15 +16,16 @@ const navBtns = document.querySelectorAll('.nav-btn')
 
 let currentPage
 
-function handleClick(e) {
-    const targetPage = e.target
 
-    updatePage(targetPage)
-}
+// function handleClick(e) {
+//     const targetPage = e.target
+
+//     updatePage(targetPage)
+// }
 
 
-function updatePage(pageOption) {
+// function updatePage(pageOption) {
 
-}
+// }
 
-navBtns.forEach((navBtn) => navBtn.addEventListener('click', handleClick))
+// navBtns.forEach((navBtn) => navBtn.addEventListener('click', handleClick))
