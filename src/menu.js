70d329dcb17c9ba33b0ed1cc0138bb3ffb1d65c7
Ross Clay <clay.rossclay@gmail.com>
2024-06-15 10:16:57 -0500

    populates page with full menu

--- src/menu.js
+++ src/menu.js
@@ -4,10 +4,7 @@ function createMenuDOM() {
     contentHeader.textContent = 'Menu'
 
 
-    for (let i = 0; i < 3; i++) {
-        const subHeader = document.createElement('div')
-        subHeader.classList.add('sub-header')
-        content.appendChild(subHeader)
+    for (let i = 0; i < 8; i++) {
         const card = document.createElement('div')
         card.classList.add('card')
         const cardHeader = document.createElement('div');
@@ -16,56 +13,66 @@ function createMenuDOM() {
         cardContent.classList.add('card-header')
 
         switch (i) {
-            case 0:
+            case 0: {
+                const subHeader = document.createElement('div')
+                subHeader.classList.add('sub-header')
                 subHeader.textContent = 'Beverages'
                 cardHeader.textContent = 'Classic Coke'
                 cardContent.textContent = 'Carbonated soft drink with a cola flavor manufactured by the Coca-Cola Company.'
-                card.appendChild(cardHeader)
-                card.appendChild(cardContent)
+                content.appendChild(subHeader)
                 break;
+            }
             case 1:
                 cardHeader.textContent = 'Diet Coke'
-                cardContent.textContent = 'Carbonated soft drink with a cola flavor manufactured by the Coca-Cola Company.'
-                card.appendChild(cardHeader)
-                card.appendChild(cardContent)
+                cardContent.textContent = 'Sugar-free and low-calorie soft drink produced and distributed by the Coca-Cola Company. It contains artificial sweeteners instead of sugar.'
+                break
+            case 2: {
+                const subHeader = document.createElement('div')
+                subHeader.classList.add('sub-header')
+                subHeader.textContent = 'Appetizers'
+                cardHeader.textContent = 'Egg Rolls'
+                cardContent.textContent = 'A cylindrical, savory roll with shredded cabbage, chopped meat, or other fillings inside a thickly-wrapped wheat flour skin, which is fried in hot oil.'
+                content.appendChild(subHeader)
                 break;
-            case 2:
-                cardHeader.textContent = 'Diet Coke'
-                cardContent.textContent = 'Carbonated soft drink with a cola flavor manufactured by the Coca-Cola Company.'
+            }
+            case 3:
+                cardHeader.textContent = 'Fried Pickles'
+                cardContent.textContent = 'Breaded pickle chips with homemade ranch dressing.'
                 break;
+
+            case 4: {
+                const subHeader = document.createElement('div')
+                subHeader.classList.add('sub-header')
+                subHeader.textContent = 'Entrees'
+                cardHeader.textContent = 'Chicken Wings'
+                cardContent.textContent = 'Deep-fried, coated in a a vinegar-based cayenne pepper hot sauce along with celery sticks and carrot sticks, and a dip of ranch dressing.'
+                content.appendChild(subHeader)
+                break;
+            }
+            case 5:
+                cardHeader.textContent = 'Pepperoni Pizza'
+                cardContent.textContent = 'A classic combination of tomato sauce, cheese, and pepperoni that has been around for decades.'
+                break;
+            case 6: {
+                const subHeader = document.createElement('div')
+                subHeader.classList.add('sub-header')
+                subHeader.textContent = 'Desserts'
+                cardHeader.textContent = 'Bread Pudding'
+                cardContent.textContent = 'Served warm alongside a scoop of vanilla ice cream.'
+                content.appendChild(subHeader)
+                break;
+            }
+            case 7:
+                cardHeader.textContent = 'Cannoli'
+                cardContent.textContent = 'Fried pastry dough, filled with a sweet, creamy filling containing ricotta cheese.'
+                break;
+
         }
 
+        card.appendChild(cardHeader)
+        card.appendChild(cardContent)
+        content.appendChild(card)
 
-
-        // const dietCokeCard = document.createElement('div')
-        // dietCokeCard.classList.add('card')
-        // const dietCokeHeader = document.createElement('div')
-        // dietCokeHeader.classList.add('card-header')
-        // dietCokeHeader.textContent = 'Diet Coke'
-        // dietCokeCard.appendChild(dietCokeHeader)
-        // const dietCokeContent = document.createElement('div')
-        // dietCokeContent.classList.add('card-content')
-        // dietCokeContent.textContent = 'Sugar-free and low-calorie soft drink produced and distributed by the Coca-Cola Company. It contains artificial sweeteners instead of sugar.'
-        // dietCokeCard.appendChild(dietCokeContent)
-        // content.appendChild(dietCokeCard)
-
-        // // appetizer section
-        // const appetizerHeader = document.createElement('div')
-        // appetizerHeader.classList.add('sub-header')
-        // appetizerHeader.textContent = 'Appetizers'
-        // content.appendChild(appetizerHeader)
-
-        // const eggRollCard = document.createElement('div')
-        // eggRollCard.classList.add('card')
-        // const eggRollHeader = document.createElement('div')
-        // eggRollHeader.classList.add('card-header')
-        // eggRollHeader.textContent = 'Egg Roll'
-        // eggRollCard.appendChild(eggRollHeader)
-        // const eggRollContent = document.createElement('div')
-        // eggRollContent.classList.add('card-content')
-        // eggRollContent.textContent = 'A cylindrical, savory roll with shredded cabbage, chopped meat, or other fillings inside a thickly-wrapped wheat flour skin, which is fried in hot oil.'
-        // cokeCard.appendChild(cokeContent)
-        // content.appendChild(cokeCard)
     }
 }
 export { createMenuDOM }
